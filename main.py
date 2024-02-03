610a75e64b391dcdad40b4dee4996103b89b0853
Rodrigo-Coloma <rjcolgut@gmail.com>
2024-02-03 09:53:52 +0100

    gridesearchv1

--- main.py
+++ main.py
@@ -1,55 +1,248 @@
 import streamlit as st
+import sklearn
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, roc_curve, explained_variance_score, mean_squared_error, r2_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from openai import OpenAI
+from dotenv import dotenv_values
 import pandas as pd
 from pandas.api.types import is_numeric_dtype
 from io import StringIO
 import matplotlib.pyplot as plt
 import seaborn as sns
+import json
 
+# Data loading
 @st.cache_data
-def data_loading(uploaded_file,separator):
+def data_loading(uploaded_file,separator, show_table):
     if len(separator) > 0:
         raw = pd.read_csv(uploaded_file,sep=separator)
     else:
         raw = pd.read_csv(uploaded_file)
-    st.dataframe(raw)
-    st.dataframe(pd.DataFrame({"name": raw.columns, "non-nulls": len(raw)-raw.isnull().sum().values,
-                                "nulls": raw.isnull().sum().values, "type": raw.dtypes.values}))
+    if raw.columns[0] == 'Unnamed: 0':
+        raw = raw.iloc[:,1:]
+    if show_table:
+        st.dataframe(raw)
+        st.dataframe(pd.DataFrame({"name": raw.columns, "non-nulls": len(raw)-raw.isnull().sum().values,
+                                "nulls": raw.isnull().sum().values, "type": raw.dtypes.values, "unique": [len(raw[col].unique()) for col in raw.columns] }))
     return raw
+
+#Filter the table and transform the data to numeric
+def filter_tarnsform(df,selected_features,target):
+    st.session_state.data = df[selected_features + [target]].dropna()
+    to_dummy = [feature for feature in selected_features if len(st.session_state.raw[feature].unique()) < 15]
+    for feature in to_dummy:
+        dummies_df = pd.get_dummies(st.session_state.data[feature], prefix=feature, drop_first=True)
+        st.session_state.data = pd.concat([st.session_state.data, dummies_df],axis=1).drop(feature, axis=1)   
+    st.session_state.data = st.session_state.data.select_dtypes(exclude=['object'])
+    return st.session_state.data
+
+
+#Model selection prompt
+#@st.cache_data
+def model_selection(data,target):
+    api_key = dotenv_values('./.env')['GPTAPIKEY']
+    client = OpenAI(api_key=api_key)
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        temperature= 0.3,
+        messages=[{"role": "system", "content": f"For the given dataset choose the top 7 sklearn models to create a {st.session_state.approach} model for {target} as well as the method to instance the model, the recomended scaler if any for each model and the required import"},
+                {"role": "user", "content": f'''Given the dataset below which shows the first 100 rows of a dataset with a total of {len(data.index)}, create a JSON object which enumerates a set of 7 child objects.                       
+                        Each child object has four properties named "model", "method","scaler" and "import". The 7 child objects are the 7 top models of sklearn to create a {st.session_state.approach} for {target}.
+                        For each child object assign to the property named "model name" to the models name, "method" to the sklearn library method used to invoke the model, "Scaler" the 
+                        recomended scaler if any for the model and dataset, and "import" the python script used to import the required final method.
+                        ''' + '''The resulting JSON object should be in this format: [{"model":"string","method":"string","scaler": "string","import": "string"}].\n\n
+                        The dataset:\n''' +
+                        f'''{str(data.head(100))}\n\n
+                        The JSON object:\n\n'''}])
+    
+    return pd.DataFrame(json.loads(completion.choices[0].message.content))
+
+#Running every recommended model
+#@st.cache_data
+def model_testing(data,target, approach, models):
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    X= data[data.columns.drop(target)]
+    y = data[target]
+    X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=42, test_size=0.2)
+    if approach == 'classifier':
+        models['AUC'] = None
+        models['score'] = None
+    else:
+        models['rmse'] = None
+        models['r2_score'] = None
+        models['explained_variance'] = None
+    fig = plt.figure()
+    for i in models.index:
+        exec(models.loc[i,'import'])
+        if models.loc[i,['scaler']][0] is not None and models.loc[i,['scaler']][0].split('.')[-1].strip('()') in ['StandardScaler',' MinMaxScaler', 'RobustScaler' ]:
+            scaler = eval(models.loc[i,['scaler']][0].split('.')[-1].strip('()') + '()')
+            X_train_i = scaler.fit_transform(X_train)
+            X_test_i = scaler.fit_transform(X_test)
+        else:
+            X_train_i, X_test_i = X_train, X_test
+        try:
+            model = eval(f'{models.loc[i,['method']][0].split('.')[-1].strip('()')}().fit(X_train_i,y_train)')
+            if st.session_state.approach == 'classifier':
+                models.loc[i,'AUC'] = roc_auc_score(y_test,model.predict(X_test_i))
+                models.loc[i,'score'] = model.score(X_test_i, y_test)
+                fpr, tpr, thresholds = roc_curve(y_test, model.predict_proba(X_test_i)[:,1])
+                plt.plot(fpr, tpr, label=models.loc[i,'model'])
+            else:
+                models.loc[i,'rmse'] = mean_squared_error(y_test,model.predict(X_test_i),squared=False)
+                models.loc[i,'r2_score'] = r2_score(y_test,model.predict(X_test_i))
+                models.loc[i,'explained_variance'] = explained_variance_score(y_test,model.predict(X_test_i))
+
+        except:
+            pass
+    if st.session_state.approach == 'classifier':
+        plt.plot([0, 1], [0, 1],'r--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.title('Receiver operating characteristic')
+        plt.legend(loc="lower right")
+        st.pyplot(fig)
+    else:
+        sns.scatterplot(data=st.session_state.models,y='rmse',x='r2_score')
+        st.pyplot()
+
+def grid_search(test_model, models, data, iterations, approach):
+    api_key = dotenv_values('./.env')['GPTAPIKEY']
+    client = OpenAI(api_key=api_key)
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+        {
+        "role": "system",
+        "content": "For a certain machine learning model and a number of iterations. Create a parameter grid to perform a GridSearch wich approximately the given number of iterations. Answer should consist only of the requested JSON object."
+        },
+        {
+        "role": "user",
+        "content": "Create a GridSearch for a RandomForestClassifier model. This gridsearch MUST be around 30 iterations. Create a JSON object which contains 1 child object with as many properties as hyperparameters should be included in the GridSearch JSON. the product of the number of values in each property of the child object MUST be around or less than 30"
+        },
+        {
+        "role": "assistant",
+        "content": "{\n  \"param_grid\": {\n    \"n_estimators\": [10, 20],\n    \"max_depth\": [None, 5],\n    \"min_samples_split\": [2, 5],\n    \"min_samples_leaf\": [1, 2],\n    \"max_features\": [\"auto\", \"sqrt\"]\n  }\n}"
+        },
+        {
+        "role": "user",
+        "content": f"Create a GridSearch for a {test_model} model. This gridsearch MUST be around {iterations} iterations. Create a JSON object which contains 1 child object with as many properties as hyperparameters should be included in the GridSearch JSON. the product of the number of values in each property of the child object MUST be around {iterations}"
+        }
+        ],
+        temperature=0.8,
+        max_tokens=1770,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+    param_grid = eval(response.choices[0].message.content.split('grid":')[-1].strip('}'))
+    st.write(param_grid)
+    
+    X= data[data.columns.drop(target)]
+    y = data[target]
+    #X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=42, test_size=0.2)
+    #st.write(list(models.loc[models['model'] == test_model,:]['method'])[0])
+    i = models.index[models['model'] == test_model].to_list()[0]
+    test_model_df = models.loc[models['model'] == test_model,:]
+    model = eval(f"{list(test_model_df['method'])[0].split('.')[-1].strip('()')}()")
+    if list(test_model_df['scaler'])[0] is not None and list(test_model_df['scaler'])[0].split('.')[-1].strip('()') in ['StandardScaler',' MinMaxScaler', 'RobustScaler' ]:
+        scaler = eval(list(test_model_df['scaler'])[0].split('.')[-1].strip('()') + '()')
+        X = scaler.fit_transform(X)
+    grid_search = GridSearchCV(model, param_grid, cv=5)
+    grid_search.fit(X,y)
+    st.write(f"Best Parameters: {grid_search.best_params_} Best Score: {grid_search.best_score_}")
+    best_model = grid_search.best_estimator_
+    X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=42, test_size=0.2)
+
+    if st.session_state.approach == 'classifier':
+        test_model_df.loc[i,'AUC'] = roc_auc_score(y_test,best_model.predict(X_test))
+        test_model_df.loc[i,'score'] = best_model.score(X_test, y_test)
+        #fpr, tpr, thresholds = roc_curve(y_test, best_model.predict_proba(X_test_i)[:,1])
+        #plt.plot(fpr, tpr, label=models.loc[i,'model'])
+    else:
+        test_model_df.loc[i,'rmse'] = mean_squared_error(y_test,best_model.predict(X_test),squared=False)
+        test_model_df.loc[i,'r2_score'] = r2_score(y_test,best_model.predict(X_test))
+        test_model_df.loc[i,'explained_variance'] = explained_variance_score(y_test,best_model.predict(X_test))
+    st.dataframe(test_model_df)
+    return best_model, test_model_df
+
+def save_model(model_name, trained_model, model_df, selected_features):
+    model_df['model'] = model_name
+    model_df['hyperparameters'] = trained_model.get_params(deep=True)
+    model_df['features'] = str(selected_features)
+    
+    st.dataframe(model_df)
+    
+    if "my_models" not in st.session_state:
+        st.session_state.my_models = model_df
+    else:
+        pd.concat([st.session_state.my_models, model_df],axis=0)
 
 uploaded_file = st.sidebar.file_uploader('Upload your csv here')
 separator = st.sidebar.text_input('Separator',placeholder=',')
 
+if "page" not in st.session_state:
+    st.session_state.page = 0
+
+# File loading
+
 if uploaded_file is not None:
-    st.session_state.raw = data_loading(uploaded_file, separator)
-    
-    target = st.sidebar.selectbox('Target', st.session_state.raw.columns, placeholder="Choose the target")
-    features = [feature for feature in st.session_state.raw.columns if feature is not target]
+    show_table = st.checkbox('Show table', value=True)
+    st.session_state.raw = data_loading(uploaded_file, separator, show_table)
+    # Choosing target and approach
+    target = st.sidebar.selectbox('Target', st.session_state.raw.columns, placeholder="Choose the target",)
+    st.session_state.approach = st.sidebar.selectbox('Approach', ['classifier', 'predictor'], placeholder="Choose the target",)
+    features = list(st.session_state.raw.columns)
+    features.remove(target)
+    # EDA
     eda_feature = st.sidebar.selectbox('EDA', st.session_state.raw.columns, placeholder="Choose a feature")
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    fig = plt.figure()
-    st.write(str(is_numeric_dtype(st.session_state.raw[eda_feature])))
-    if is_numeric_dtype(st.session_state.raw[eda_feature]) and len(list(st.session_state.raw[eda_feature].unique())) > 15:
-        sns.boxplot(data=st.session_state.raw,y=eda_feature,hue=target, showfliers = False)
-        st.pyplot()
-    else:
-        sns.countplot(data=st.session_state.raw,y=eda_feature,hue=target)
-        st.pyplot()
-        if st.sidebar.button("One Hot Encode"):
-            if 'to_dummy' not in st.session_state:
-                st.session_state.to_dummy = ['eda_feature']
+    if st.sidebar.button('Plot'):
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        fig = plt.figure()
+        if st.session_state.approach == 'classifier'    :
+            if is_numeric_dtype(st.session_state.raw[eda_feature]) and len(list(st.session_state.raw[eda_feature].unique())) > 15:
+                sns.boxplot(data=st.session_state.raw,y=eda_feature,hue=target, showfliers = False)           
             else:
-                st.session_state.to_dummy.append('eda_feature')
-
-target_features = st.sidebar.multiselect('Selected Features',features)
-
-
-
+                sns.countplot(data=st.session_state.raw,y=eda_feature,hue=target)            
+        else:
+            if is_numeric_dtype(st.session_state.raw[eda_feature]) and len(list(st.session_state.raw[eda_feature].unique())) > 15:
+                sns.scatterplot(data=st.session_state.raw,x=eda_feature,y=target)           
+            else:
+                sns.boxplot(data=st.session_state.raw,y=target,hue=eda_feature, showfliers = False)
+        st.pyplot()
+# Feature selection
+selected_features = st.sidebar.multiselect('Selected Features',features)
 if st.sidebar.button('Filter and transform'):
-    st.session_state.data = st.session_state.raw[target_features].dropna()
-    to_dummy = [feature for feature in target_features if len(list(st.session_state.raw[feature].unique())) < 15]
-    for feature in to_dummy:
-        dummies_df = pd.get_dummies(st.session_state.data[feature], prefix=feature, drop_first=True)
-        st.session_state.data= pd.concat([st.session_state.data, dummies_df],axis=1).drop(feature, axis=1)
+    st.session_state.data = filter_tarnsform(st.session_state.raw,selected_features,target)
+if st.checkbox('Show Data',value=True) and "data" in st.session_state:
     st.dataframe(st.session_state.data)
-    st.dataframe(pd.DataFrame({"name": st.session_state.data.columns, "non-nulls": len(st.session_state.data)-st.session_state.data.isnull().sum().values, "nulls": st.session_state.data.isnull().sum().values, "type": st.session_state.data.dtypes.values}))
-        
+    st.dataframe(pd.DataFrame({"name": st.session_state.data.columns, "non-nulls": len(st.session_state.data)-st.session_state.data.isnull().sum().values, "nulls": st.session_state.data.isnull().sum().values, "type": st.session_state.data.dtypes.values, "unique": [len(st.session_state.data[col].unique()) for col in st.session_state.data.columns] }))
+
+# model recommendation   
+if st.sidebar.button('Recomended models',type='primary'):
+    st.session_state.models = model_selection(st.session_state.data, target)
+if st.checkbox('Show recommended models', value=True):    
+    st.dataframe(st.session_state.models)
+# model testing
+if st.sidebar.button('Test Models',type='primary'):
+    model_testing(st.session_state.data,target, st.session_state.approach, st.session_state.models)
+# Gridsearch
+if "models" in st.session_state:
+    test_model = st.sidebar.selectbox(' Test Model ', st.session_state.models['model'], placeholder="Choose the model")
+    iterations = st.sidebar.slider('Iterations', 20, 1000, 20)
+    if st.sidebar.button('Gridsearch'):
+        st.session_state.trained_model, st.session_state.test_model_df = grid_search(test_model,st.session_state.models, st.session_state.data, iterations, st.session_state.approach)
+    if st.session_state.test_model_df is not None:
+        model_name = st.text_input('model name')
+        if st.button('Save model'):
+            save_model(model_name, st.session_state.trained_model, st.session_state.test_model_df, selected_features)
+if st.checkbox('My models') and "my_models" in st.session_state:
+    st.dataframe(st.session_state.my_models)
+
